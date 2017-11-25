65d3bab3d3d44903dcefdc00c1eff61241775d5a
MamoshiSE <33344169+MamoshiSE@users.noreply.github.com>
2017-11-26 00:10:13 +0100

    Update Libary.java

--- Libary.java
+++ Libary.java
@@ -2,27 +2,41 @@ package Libary;
 
 import java.util.ArrayList;
 
-
-
-
-
 public class Libary {
 	ArrayList<Customer> customers;
+	ArrayList<Books> books;
+	ArrayList<Books> borrowedBooks;
 
 	public Libary() {
 
 		this.customers = new ArrayList<Customer>();
+		this.books = new ArrayList<Books>();
+		this.borrowedBooks = new ArrayList<Books>();
 
 	}
-	
-	public void addCustomer(String name, int phoneNumber, String streetName, int streetNum, int zipCode, String city, String country) {
+
+	public void addCustomer(String name, int phoneNumber, String streetName, int streetNum, int zipCode, String city,
+			String country) {
 
 		Customer cus = new Customer(name, phoneNumber, streetName, streetNum, zipCode, city, country);
-		
-			this.customers.add(cus);
 
+		this.customers.add(cus);
+
+	}
+
+	public void addBook(String title, String author, String genre, String publisher, char shelf) {
+		Books book = new Books(title, author, genre, publisher, shelf);
+		this.books.add(book);
+	}
+
+	public void retrieveBookList() {
+		for (int i = 0; i < books.size(); i++) {
+			System.out.print(books.get(i).getTitle());
 		}
 	}
 	
-
-
+	public void BorrowBook(String name)
+	{
+		// this.customers.setName(name);
+	}
+}
